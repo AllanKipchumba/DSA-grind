7c303e21706d0bf5ab57d053ed4d9c7be97f683f
allan <allankipchumba76@gmail.com>
2023-11-29 12:06:21 +0300

    add the following methhods on the singlyLinkedList file: shifting, unshifting,get, set, insert,remove, and reverse

--- 5. Data Structures/1 SinglyLinkedList.js
+++ 5. Data Structures/1 SinglyLinkedList.js
@@ -22,7 +22,7 @@ class SinglyLinkedList {
     this.length = 0;
   }
 
-  //adding a new node to the end of the linked list
+  //adding a new node to the end of a linked list
   push(val) {
     let newNode = new Node(val);
     if (!this.head) {
@@ -57,5 +57,122 @@ class SinglyLinkedList {
     }
 
     return current;
+  }
+
+  //removing a new node from the begining of the linkedlist
+  shift() {
+    if (!this.head) return undefined;
+
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+
+    if (this.length === 0) {
+      this.tail = null;
+    }
+
+    return currentHead;
+  }
+
+  //adding a new node to the beginning of the linkedlist
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  //retrieve a node by its position in the linkedlist
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let counter = 0;
+    let current = this.head;
+
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+
+    return current;
+  }
+
+  //change the value of a node based on its position
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  //adding a node to the linkedlist at a specific position
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+
+    this.length++;
+    return true;
+  }
+
+  //remove a node from the linked list at a specific position
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === this.length - 1) return this.pop();
+    if (index === 0) return this.shift();
+
+    let prevNode = this.get(index - 1);
+    let targetNode = prevNode.next;
+    prevNode.next = targetNode.next;
+
+    this.length--;
+    return targetNode;
+  }
+
+  //***reverse a SinglyLinkedList in place***
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+
+    let next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+
+  //see the values in the linkedlist
+  print() {
+    let arr = [];
+    let current = this.head;
+
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+
+    console.log(arr);
   }
 }
