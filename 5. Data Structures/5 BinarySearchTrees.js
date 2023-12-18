6943db77746f04c53a7eeec53ccc29aa4033d547
allan <allankipchumba76@gmail.com>
2023-12-18 03:47:43 +0300

    Implement Tree traversal in the BST-DS. BFS and DFS-(PreOrder,PostOrder,InOrder) methods implemented.

--- 5. Data Structures/5 BinarySearchTrees.js
+++ 5. Data Structures/5 BinarySearchTrees.js
@@ -1,5 +1,5 @@
 /**
- * A tree is a none-linear data structure that consists of nodes in a parent-child relationship.
+ * A tree is a non-linear data structure that consists of nodes in a parent-child relationship.
  * Trees - A parent node can have as many child nodes as it can
  * Binary trees - Each node can have a maximum of 2 children
  * Binary search tree - Special kind of a binary tree that is sorted in a particular order
@@ -96,9 +96,82 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
+  //TREE TRAVERSAL - printing the nodes in a tree
+  // (1) Breadth-first-search(BFS) => visit the root, every sibling node, then look at the child
+  BFS() {
+    let node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  // (2) Depth-first-search(DFS) => Traverse nodes vertically down to the end of the tree
+  //                                before visiting sibling nodes.
+  //     There are three forms of DFS
+  // (i) DFS-PreOrder => Visit the node first, traverse the left, then traverse the right
+  DFSPreOrder() {
+    let data = [];
+
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
+
+  // (ii) DFS-PostOrder => Explore all children before we visit the node. The root is the
+  //                       last to be visited
+  DFSPostOrder() {
+    let data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+
+    traverse(this.root);
+    return data;
+  }
+
+  // DFS-InOrder => Traverse the entire left side, visit the node, then traverse the entire right side
+  DFSInOrder() {
+    let data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
 
+// tree.insertI(10);
+// tree.insertR(7);
+//         10
+//     6       15
+//   3   8         20
+
 tree.insertI(10);
-tree.insertR(7);
+tree.insertI(6);
+tree.insertI(15);
+tree.insertI(3);
+tree.insertI(8);
+tree.insertI(20);
